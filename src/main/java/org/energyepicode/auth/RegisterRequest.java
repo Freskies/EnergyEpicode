08ad966841b2198e0c657abbb158053d5a62c8bf
GiuseppeDot <giuseppegianni190@gmail.com>
2025-02-24 13:05:28 +0100

    auth

---
+++ src/main/java/org/energyepicode/auth/RegisterRequest.java
@@ -0,0 +1,9 @@
+package org.energyepicode.auth;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/org/energyepicode/auth/Role.java
@@ -0,0 +1,7 @@
+package org.energyepicode.auth;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN,
+    ROLE_SELLER
+}
