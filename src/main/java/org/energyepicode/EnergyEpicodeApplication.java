dc3a6fde893f7d1275ace3f0103fe9b66a38fd19
Freskies <43386854+Freskies@users.noreply.github.com>
2025-02-24 10:00:13 +0100

    Initial commit

---
+++ src/main/java/org/energyepicode/EnergyEpicodeApplication.java
@@ -0,0 +1,13 @@
+package org.energyepicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyEpicodeApplication {
+
+	public static void main (String[] args) {
+		SpringApplication.run(EnergyEpicodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/energyepicode/EnergyEpicodeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.energyepicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyEpicodeApplicationTests {
+
+	@Test
+	void contextLoads () {
+	}
+
+}
